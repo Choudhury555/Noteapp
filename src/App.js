940a4f350670d45993b74b651f592b6dc0661d5c
Choudhury555 <choudhurypanda143@gmail.com>
2023-08-23 01:06:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [notes, setNotes] = React.useState(
+        () => JSON.parse(localStorage.getItem("notes")) || []
+    )
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+    
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    function updateNote(text) {
+        //put the recently modified notes to the top
+        setNotes((oldNotes) => {
+            const newArray=[]
+            for(let i=0;i<oldNotes.length;i++){
+                const oldNote=oldNotes[i]
+                if(oldNote.id===currentNoteId){
+                    newArray.unshift({ ...oldNote, body: text })
+                }
+                else{
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+        //Below code will not rearrange the recently modified notes to the top
+        // setNotes(oldNotes => oldNotes.map(oldNote => {
+        //     return oldNote.id === currentNoteId
+        //         ? { ...oldNote, body: text }
+        //         : oldNote
+        // }))  
+    }
+    
+    
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        // Your code here
+        console.log("deleted")
+        setNotes((oldNotes) => {
+            const newArray=[]
+            for(let i=0;i<oldNotes.length;i++){
+                const oldNote=oldNotes[i]
+                if(oldNote.id === noteId){
+                    continue
+                }
+                else{
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+    }
+    
+    
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
 }
-
-export default App;
